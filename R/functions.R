2fae625d9b3ccca5141ef2369a6fe682a9d9d54f
Jesper Frandsen <jsbfrandsen@gmail.com>
2024-12-11 09:46:03 +0100

    created a function and a targets

---
+++ R/functions.R
@@ -0,0 +1,20 @@
+#' Descriptive stats
+#'
+#' @param data
+#'
+#' @return A data frame/tibble
+descriptive_stats <- function(data) {
+    data %>%
+        dplyr::group_by(metabolite) %>%
+        dplyr::summarise(dplyr::across(
+            c(value),
+            list(
+                mean = mean,
+                sd = sd
+            )
+        )) %>%
+        dplyr::mutate(dplyr::across(
+            where(is.numeric),
+            ~ round(.x, digits = 1)
+        ))
+}
